2ce24212ccec1b5f8f03e9fcf3a2d637b5274b80
Rocky Truong <Rockytruong19@gmail.com>
2021-07-28 00:58:16 +0000

    Create a dummy component for the header.jsx and search.jsx to instantly display pages to work with. #1

--- client/app.jsx
+++ client/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import Home from './pages/home';
+import Search from './pages/search';
+import Header from './components/header';
 
 export default class App extends React.Component {
   render() {
-    return <Home />;
+    return (
+      <>
+      <Header />
+      <Search />
+      </>
+    );
   }
 }
