f90b3e717adf56129622dde0c9bdd929857b06b7
Rocky Truong <Rockytruong19@gmail.com>
2021-07-29 18:52:05 +0000

    Update schema.sql to have a table for messages with columns for two userId, a unique messageId, and message. #1

--- database/schema.sql
+++ database/schema.sql
@@ -6,12 +6,32 @@ drop schema "public" cascade;
 
 create schema "public";
 
-create table "public"."users" (
-	"userId"      serial  NOT NULL,
-	"name"        TEXT    NOT NULL,
-	"city"        TEXT    NOT NULL,
-	"ntrpRating"  integer  NOT NULL,
+CREATE TABLE "public"."users" (
+	"userId" serial NOT NULL,
+	"name" TEXT NOT NULL,
 	"createdAt" timestamptz(6) not null default now(),
-	constraint "users_pk"
-  primary key ("userId")
+	"city" TEXT NOT NULL,
+	"ntrpRating" integer NOT NULL,
+	CONSTRAINT "users_pk" PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
 );
+
+
+
+CREATE TABLE "public"."messages" (
+	"messageId" serial NOT NULL,
+	"message" TEXT NOT NULL,
+	"createdAt" timestamptz(6) not null default now(),
+	"senderId" serial NOT NULL,
+	"recipientId" serial NOT NULL,
+	CONSTRAINT "messages_pk" PRIMARY KEY ("messageId")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "messages" ADD CONSTRAINT "messages_fk0" FOREIGN KEY ("senderId") REFERENCES "users"("userId");
+ALTER TABLE "messages" ADD CONSTRAINT "messages_fk1" FOREIGN KEY ("recipientId") REFERENCES "users"("userId");
