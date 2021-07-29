3d7f25a73a8689025f43344ed6a0cf3291574e71
Rocky Truong <Rockytruong19@gmail.com>
2021-07-29 22:31:13 +0000

    Update index.js to post data to the database for each message. #2

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const errorMiddleware = require('./error-middleware');
 const staticMiddleware = require('./static-middleware');
 const pg = require('pg');
+const ClientError = require('./client-error');
 const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
@@ -13,15 +14,13 @@ const app = express();
 
 app.use(staticMiddleware);
 
-app.use(errorMiddleware);
-
 app.get('/api/users', (req, res, next) => {
   const sql = `
-    select "name",
-           "city",
-           "ntrpRating",
-           "userId"
-      from "users"
+  select "name",
+  "city",
+  "ntrpRating",
+  "userId"
+  from "users"
   `;
   db.query(sql)
     .then(result => {
@@ -29,6 +28,32 @@ app.get('/api/users', (req, res, next) => {
     })
     .catch(err => next(err));
 });
+
+app.use(express.json());
+
+app.post('/api/messages', (req, res, next) => {
+  const newMessage = req.body;
+  const sender = parseInt(newMessage.senderId, 10);
+  const recipient = parseInt(newMessage.recipientId, 10);
+  const message = newMessage.message;
+  const sql = `
+  insert into "messages" ("senderId", "recipientId", "message")
+  values ($1, $2, $3)
+  returning *
+  `;
+  const params = [sender, recipient, message];
+  if (!sender || !recipient || !message) {
+    throw new ClientError(400, 'senderId, recipientId, or message is missing');
+  }
+  db.query(sql, params)
+    .then(result => {
+      const postedMessage = result.rows[0];
+      res.status(201).json(postedMessage);
+    })
+    .catch(err => next(err));
+});
+
+app.use(errorMiddleware);
 
 app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
