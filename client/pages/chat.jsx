4431d866fb895f8fba012f7b32795c6d5db842c0
Rocky Truong <Rockytruong19@gmail.com>
2021-07-30 22:59:27 +0000

    Fixed component to display just the page header if there are no messages. #2

--- client/pages/chat.jsx
+++ client/pages/chat.jsx
@@ -4,25 +4,54 @@ export default class Chat extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: null
+      messages: []
     };
   }
 
   componentDidMount() {
-    fetch('/api/messages/1')
+    fetch(`/api/messages/${this.props.recipientId}`)
       .then(response => response.json())
       .then(messages => this.setState({ messages }));
   }
 
   render() {
-    if (!this.state.messages) return null;
-    const { message, createdAt, senderId, recipientId } = this.state.messages;
-    console.log(message);
-    return (
+    const messages = this.state.messages;
+    if (messages.length >= 1) {
+      return (
       <>
-      <div>testing</div>
-      <div>{message}</div>
+      <div className="container">
+        <h2 className="page-header">Chat with {this.props.recipientId}</h2>
+        <div>
+          {
+            messages.map(message => {
+              return (
+              <Message
+              key={message.messageId}
+              message={message} />
+              );
+            })
+        }
+        </div>
+      </div>
       </>
-    );
+      );
+    } else {
+      return (
+        <>
+          <div className="container">
+            <h2 className="page-header">Chat with {this.props.recipientId}</h2>
+          </div>
+        </>
+      );
+    }
   }
+}
+
+function Message(props) {
+  const { message } = props.message;
+  return (
+    <div>
+      { message }
+    </div>
+  );
 }
