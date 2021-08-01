3cbca7d9b72a31e609c3edf1174f381f44874932
Rocky Truong <Rockytruong19@gmail.com>
2021-08-01 11:08:01 +0000

    Added search button and search link to return to search results from direct message. #2

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -8,6 +8,17 @@ export default class Header extends React.Component {
           <div className="container">
             <div className="header">
               <div className="title">Tennis Buddy</div>
+              <div className="links">
+                <a className="search-link" href="#">
+                  Search
+                </a>
+                <a className="chats-link">
+                  Chats
+                </a>
+              </div>
+              <a className="search-icon" href="#">
+                <i className="fas fa-search fa-3x"></i>
+              </a>
             </div>
           </div>
         </div>
